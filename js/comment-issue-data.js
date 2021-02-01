26c54c9e26a2e6db524dc47c5abe127a98ea917e
MrDemonlxl <714416426@qq.com>
2021-02-01 19:47:40 +0800

    Site updated: 2021-02-01 19:47:36

--- js/comment-issue-data.js
+++ js/comment-issue-data.js
@@ -1,17 +1,17 @@
-// 评论issues仓库 by.removeif https://removeif.github.io/
-var repoIssuesUrl = "https://api.github.com/repos/userName/userRepo/issues";
+// 评论issues仓库 
+var repoIssuesUrl = "https://api.github.com/repos/MrDemonlxl/blog_comment/issues";
 // 对应仓库 clientId、clientSecret 关于这两个参数的安全问题，查看 https://removeif.github.io/2019/09/19/博客源码分享.html#1-热门推荐，最新评论：
-var clientId;// = "46a9f3481b46ea0129d8";
-var clientSecret;// = "79c7c9cb847e141757d7864453bcbf89f0655b24";
-// var authorizationToken = 'Basic ' + btoa(clientId + ':' + clientSecret);
+var clientId = "20ef8321698ab8c0c37b";
+var clientSecret = "237515b7faa3e1097f2980762f772c6f02dbcbbb";
+var authorizationToken = 'Basic ' + btoa(clientId + ':' + clientSecret);
 // 写comment count值
-var reqCommentCountUrl;
+var reqCommentCountUrl = repoIssuesUrl + "?t=" + new Date().getTime() + "&labels=Gitalk,";
 // 评论缓存key
 var COMMENT_CACHE_KEY = "commentKey";
 var valine;
 
 // 管理员名称,评论时添加 [博主] 后缀
-var ADMIN_NAME = "removeif";
+var ADMIN_NAME = "MrDemonlxl";
 
 function ajaxReqForGitHub(url, authorizationToken, call) {
     $.ajax({
